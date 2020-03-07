fb594db769b26183c3e318448842e2cf557d3296
jaisalsabu <jaisal1010sabu@gmail.com>
2020-03-07 15:04:30 +0530

    instagramlikebutton

--- app/src/main/java/com/example/feedpage/MainActivity.java
+++ app/src/main/java/com/example/feedpage/MainActivity.java
@@ -3,12 +3,75 @@ package com.example.feedpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView heartAnim;
+    ImageView likeImg;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trollrecycler);
+        imageView=findViewById(R.id.imageView);
+        heartAnim = (ImageView) findViewById(R.id.imageView01);
+        likeImg = (ImageView) findViewById(R.id.imageView02);
+        final GestureDetector gd = new GestureDetector(MainActivity.this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+
+                Animation pulse_fade = AnimationUtils.loadAnimation(MainActivity.this, R.anim.pulsefadein);
+                pulse_fade.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        heartAnim.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        heartAnim.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                heartAnim.startAnimation(pulse_fade);
+                likeImg.setImageDrawable(MainActivity.this.getResources().getDrawable(R.drawable.likeactive));
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                super.onLongPress(e);
+
+            }
+
+            @Override
+            public boolean onDoubleTapEvent(MotionEvent e) {
+                return true;
+            }
+        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                return gd.onTouchEvent(event);
+            }
+        });
     }
-}
+
+
+    }
